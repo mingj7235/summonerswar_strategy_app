63ea4469742a523e9d091a8d234f1a12cb248427
mingj7235 <joshuara7235@gmail.com>
2022-05-04 16:00:49 +0900

    Test Controller for mapping

---
+++ src/main/java/com/joshua/summonerswar/domain/member/controller/MemberController.java
@@ -0,0 +1,20 @@
+package com.joshua.summonerswar.domain.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    @GetMapping ("/test")
+    @ResponseBody
+    public String test () {
+
+        return "hello monkeys";
+    }
+}
