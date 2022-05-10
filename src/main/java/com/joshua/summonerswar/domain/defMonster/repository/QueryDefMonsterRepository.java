5dfa4e01b38066ff8c167dacbc9a5e746cad23d9
mingj7235 <joshuara7235@gmail.com>
2022-05-10 17:40:32 +0900

    Leader Monster 검색 및 페이지네이션 QueryDsl 작성

--- src/main/java/com/joshua/summonerswar/domain/defMonster/repository/QueryDefMonsterRepository.java
+++ src/main/java/com/joshua/summonerswar/domain/defMonster/repository/QueryDefMonsterRepository.java
@@ -1,4 +1,14 @@
 package com.joshua.summonerswar.domain.defMonster.repository;
 
+import com.joshua.summonerswar.domain.defMonster.dto.response.DefMonsterResponseDto;
+import com.joshua.summonerswar.domain.defMonster.entity.DefMonsters;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import javax.validation.constraints.NotBlank;
+
 public interface QueryDefMonsterRepository {
+
+    Page<DefMonsters> findByLeaderMonster (final @NotBlank String leaderMonster,
+                                           Pageable pageable);
 }
--- src/main/java/com/joshua/summonerswar/domain/defMonster/repository/QueryDefMonsterRepositoryImpl.java
+++ src/main/java/com/joshua/summonerswar/domain/defMonster/repository/QueryDefMonsterRepositoryImpl.java
@@ -1,10 +1,48 @@
 package com.joshua.summonerswar.domain.defMonster.repository;
 
+import com.joshua.summonerswar.domain.defMonster.dto.response.DefMonsterResponseDto;
+import com.joshua.summonerswar.domain.defMonster.entity.DefMonsters;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
+
+import javax.validation.constraints.NotBlank;
+
+import java.util.List;
+
+import static com.joshua.summonerswar.domain.defMonster.entity.QDefMonsters.defMonsters;
 
 @RequiredArgsConstructor
 public class QueryDefMonsterRepositoryImpl implements QueryDefMonsterRepository{
 
     private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public Page<DefMonsters> findByLeaderMonster(final @NotBlank String leaderMonster,
+                                                 Pageable pageable) {
+        List<DefMonsters> result = jpaQueryFactory.selectFrom(defMonsters)
+                .where(
+                        leaderMonsterLike(leaderMonster)
+                )
+                .orderBy(defMonsters.createdDate.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        JPAQuery<DefMonsters> countQuery = jpaQueryFactory
+                .selectFrom(defMonsters)
+                .where(
+                        leaderMonsterLike(leaderMonster)
+                );
+
+        return PageableExecutionUtils.getPage(result, pageable, countQuery::fetchCount);
+    }
+
+    private BooleanExpression leaderMonsterLike (String leaderMonster) {
+        return !leaderMonster.isEmpty() ? defMonsters.leaderMonster.contains(leaderMonster) : null;
+    }
 }
