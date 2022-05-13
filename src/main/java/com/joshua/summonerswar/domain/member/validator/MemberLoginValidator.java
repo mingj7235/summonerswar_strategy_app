332ad1b2d7daf6c19949f7270140c18df2cb3be9
mingj7235 <joshuara7235@gmail.com>
2022-05-13 15:36:19 +0900

    Login validator bean 등록 (내부 logic 작성 필요)

--- src/main/java/com/joshua/summonerswar/domain/member/validator/MemberLoginValidator.java
+++ src/main/java/com/joshua/summonerswar/domain/member/validator/MemberLoginValidator.java
@@ -1,8 +1,14 @@
 package com.joshua.summonerswar.domain.member.validator;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class MemberLoginValidator implements Validator {
 
     @Override
