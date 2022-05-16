fe4c629f2cf1f844c0e3adeb5097d8a2f92cc3a1
mingj7235 <joshuara7235@gmail.com>
2022-05-16 18:18:22 +0900

    Attack monster domain controller 뼈대 점검

--- src/main/java/com/joshua/summonerswar/domain/attackMonster/controller/AttackMonsterController.java
+++ src/main/java/com/joshua/summonerswar/domain/attackMonster/controller/AttackMonsterController.java
@@ -1,11 +1,27 @@
 package com.joshua.summonerswar.domain.attackMonster.controller;
 
+import com.joshua.summonerswar.domain.attackMonster.dto.request.AttackMonsterRequestDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.constraints.NotNull;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class AttackMonsterController {
+
+    @GetMapping ("/atk/register")
+    public String viewAtkRegister (Model model) {
+        return "atk/register";
+    }
+
+    @PostMapping ("/atk/register")
+    public String atkRegister (final AttackMonsterRequestDto.@NotNull Register register) {
+        return "redirect:/atk/register";
+    }
 }
