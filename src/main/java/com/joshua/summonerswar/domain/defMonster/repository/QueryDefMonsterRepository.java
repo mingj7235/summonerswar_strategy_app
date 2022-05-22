d6805f9895a5b2fbdd585ced3ab2504102be7d79
김민재 <joshuara7235@gmail.com>
2022-05-22 22:11:05 +0900

    1. find 로직변경 (추후에 검색조건 따라 변경해야함) -> querydsl 로직 분리해야함
    2. find view 코드 변경

--- src/main/java/com/joshua/summonerswar/domain/defMonster/repository/QueryDefMonsterRepository.java
+++ src/main/java/com/joshua/summonerswar/domain/defMonster/repository/QueryDefMonsterRepository.java
@@ -1,17 +1,16 @@
 package com.joshua.summonerswar.domain.defMonster.repository;
 
+import com.joshua.summonerswar.domain.defMonster.dto.request.DefMonsterRequestDto;
 import com.joshua.summonerswar.domain.defMonster.dto.response.DefMonsterResponseDto;
 import com.joshua.summonerswar.domain.defMonster.entity.DefMonsters;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 public interface QueryDefMonsterRepository {
 
-    Page<DefMonsters> findByLeaderMonster (final @NotBlank String leaderMonster,
-                                           Pageable pageable);
-
-    Page<DefMonsters> findByKeyword (final @NotBlank String keyword,
+    Page<DefMonsters> findByKeyword (final @NotNull DefMonsterRequestDto.Find request,
                                      Pageable pageable);
 }
--- src/main/java/com/joshua/summonerswar/domain/defMonster/repository/QueryDefMonsterRepositoryImpl.java
+++ src/main/java/com/joshua/summonerswar/domain/defMonster/repository/QueryDefMonsterRepositoryImpl.java
@@ -1,6 +1,6 @@
 package com.joshua.summonerswar.domain.defMonster.repository;
 
-import com.joshua.summonerswar.domain.defMonster.dto.response.DefMonsterResponseDto;
+import com.joshua.summonerswar.domain.defMonster.dto.request.DefMonsterRequestDto;
 import com.joshua.summonerswar.domain.defMonster.entity.DefMonsters;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQuery;
@@ -10,23 +10,23 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.support.PageableExecutionUtils;
 
-import javax.validation.constraints.NotBlank;
-
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 import static com.joshua.summonerswar.domain.defMonster.entity.QDefMonsters.defMonsters;
 
 @RequiredArgsConstructor
-public class QueryDefMonsterRepositoryImpl implements QueryDefMonsterRepository{
+public class QueryDefMonsterRepositoryImpl implements QueryDefMonsterRepository {
 
     private final JPAQueryFactory jpaQueryFactory;
 
     @Override
-    public Page<DefMonsters> findByLeaderMonster(final @NotBlank String leaderMonster,
-                                                 Pageable pageable) {
+    public Page<DefMonsters> findByKeyword(final @NotNull DefMonsterRequestDto.Find request, final Pageable pageable) {
+
         List<DefMonsters> result = jpaQueryFactory.selectFrom(defMonsters)
                 .where(
-                        leaderMonsterLike(leaderMonster)
+                        leaderMonsterLike(request.getLeaderMonster()),
+                        otherMonsterLike(request.getKeyword())
                 )
                 .orderBy(defMonsters.createdDate.desc())
                 .offset(pageable.getOffset())
@@ -36,39 +36,17 @@ public class QueryDefMonsterRepositoryImpl implements QueryDefMonsterRepository{
         JPAQuery<DefMonsters> countQuery = jpaQueryFactory
                 .selectFrom(defMonsters)
                 .where(
-                        leaderMonsterLike(leaderMonster)
                 );
 
         return PageableExecutionUtils.getPage(result, pageable, countQuery::fetchCount);
     }
 
-    @Override
-    public Page<DefMonsters> findByKeyword(final String keyword, final Pageable pageable) {
-
-        List<DefMonsters> result = jpaQueryFactory.selectFrom(defMonsters)
-                .where(
-                        keywordLike(keyword)
-                )
-                .orderBy(defMonsters.createdDate.desc())
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize())
-                .fetch();
-
-        JPAQuery<DefMonsters> countQuery = jpaQueryFactory
-                .selectFrom(defMonsters)
-                .where(
-                        keywordLike(keyword)
-                );
-
-        return PageableExecutionUtils.getPage(result, pageable, countQuery::fetchCount);
-    }
-
-    private BooleanExpression leaderMonsterLike (String leaderMonster) {
+    private BooleanExpression leaderMonsterLike(String leaderMonster) {
         return !leaderMonster.isEmpty() ? defMonsters.leaderMonster.contains(leaderMonster) : null;
     }
 
-    private BooleanExpression keywordLike (String keyword) {
-        return !keyword.isEmpty() ? defMonsters.leaderMonster.contains(keyword) :
-                defMonsters.otherMonster.contains(keyword);
+    private BooleanExpression otherMonsterLike(String keyword) {
+        return !keyword.isEmpty() ? defMonsters.otherMonster.contains(keyword) : null;
     }
+
 }
