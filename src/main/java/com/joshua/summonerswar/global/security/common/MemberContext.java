d5c1dddb00993fe85c963f07ebaef7fd18f9ecf0
mingj7235 <joshuara7235@gmail.com>
2022-10-19 10:51:43 +0900

    feat : Login 된 사용자의 정보를 navigation bar 에 노출

--- src/main/java/com/joshua/summonerswar/global/security/common/MemberContext.java
+++ src/main/java/com/joshua/summonerswar/global/security/common/MemberContext.java
@@ -2,12 +2,16 @@ package com.joshua.summonerswar.global.security.common;
 
 import com.joshua.summonerswar.domain.member.entity.Member;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
+import javax.validation.Valid;
 import java.util.Collection;
 
 @Getter
+@Setter
+@Valid
 public class MemberContext extends User {
 
     private final Member member;
