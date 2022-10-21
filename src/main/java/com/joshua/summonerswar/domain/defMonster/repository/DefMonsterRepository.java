bb2ac4a54e9b5d3bcad8a8c4397c968f4f9a15a2
mingj7235 <joshuara7235@gmail.com>
2022-10-21 15:18:54 +0900

    refactor : 안쓰는 패키지 정리

--- src/main/java/com/joshua/summonerswar/domain/defMonster/repository/DefMonsterRepository.java
+++
@@ -1,8 +0,0 @@
-package com.joshua.summonerswar.domain.defMonster.repository;
-
-import com.joshua.summonerswar.domain.defMonster.entity.DefMonsters;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DefMonsterRepository extends JpaRepository<DefMonsters, Long>, QueryDefMonsterRepository {
-
-}
