1807ca548463dce6b18567780d23e792a1f48759
mingj7235 <joshuara7235@gmail.com>
2022-11-16 17:40:50 +0900

    feat : 댓글 구현을 위한 Reply Entity 설계 - 무한 댓글

---
+++ src/main/java/com/joshua/summonerswar/domain/reply/entity/Reply.java
@@ -0,0 +1,40 @@
+package com.joshua.summonerswar.domain.reply.entity;
+
+import com.joshua.summonerswar.domain.member.entity.Member;
+import com.joshua.summonerswar.global.base.BaseTime;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter(AccessLevel.PRIVATE)
+@SuperBuilder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reply extends BaseTime {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String boardName;
+
+    private String replyMsg;
+
+    private Integer depth;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne (fetch = FetchType.LAZY)
+    @JoinColumn (name = "parent_reply_id")
+    private Reply parentReply;
+
+    @OneToMany (mappedBy = "parentReply", cascade = CascadeType.ALL)
+    private List<Reply> subReplyList = new ArrayList<>();
+}
---
+++ src/main/java/com/joshua/summonerswar/domain/reply/service/ReplyService.java
@@ -0,0 +1,13 @@
+package com.joshua.summonerswar.domain.reply.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ReplyService {
+}
