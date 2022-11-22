7d0d96e9b9f3ad27cb2d6ba96b9ec42ea63f3b6f
joshua <joshuara7235@gmail.com>
2022-11-22 22:03:53 +0900

    feat: 공덱 저장 Back 로직

--- src/main/java/com/joshua/summonerswar/domain/siege/entity/relation/MonsterAtkDeck.java
+++ src/main/java/com/joshua/summonerswar/domain/siege/entity/relation/MonsterAtkDeck.java
@@ -2,15 +2,16 @@ package com.joshua.summonerswar.domain.siege.entity.relation;
 
 import com.joshua.summonerswar.domain.monster.entity.Monster;
 import com.joshua.summonerswar.domain.siege.entity.AtkDeck;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter (AccessLevel.PRIVATE)
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MonsterAtkDeck {
 
     @Id
@@ -24,5 +25,12 @@ public class MonsterAtkDeck {
     @ManyToOne
     @JoinColumn (name = "atk_deck_id")
     private AtkDeck atkDeck;
+
+    public static MonsterAtkDeck toEntity(final AtkDeck atkDeck, final Monster monster) {
+        return MonsterAtkDeck.builder()
+                .monster(monster)
+                .atkDeck(atkDeck)
+                .build();
+    }
 
 }
--- src/main/java/com/joshua/summonerswar/domain/siege/service/core/relation/RelMonsterAtkDeckService.java
+++ src/main/java/com/joshua/summonerswar/domain/siege/service/core/relation/RelMonsterAtkDeckService.java
@@ -1,5 +1,30 @@
 package com.joshua.summonerswar.domain.siege.service.core.relation;
 
+import com.joshua.summonerswar.domain.monster.entity.Monster;
+import com.joshua.summonerswar.domain.siege.entity.AtkDeck;
+import com.joshua.summonerswar.domain.siege.entity.DefDeck;
+import com.joshua.summonerswar.domain.siege.entity.relation.MonsterAtkDeck;
+import com.joshua.summonerswar.domain.siege.entity.relation.MonsterDefDeck;
+import com.joshua.summonerswar.domain.siege.repository.relation.RelMonsterAtkDeckRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class RelMonsterAtkDeckService {
+
+    private final RelMonsterAtkDeckRepository relMonsterAtkDeckRepository;
+
+    public void registerDefDeck(AtkDeck atkDeck, List<Monster> monsters) {
+        monsters.forEach(monster -> {
+            relMonsterAtkDeckRepository.save(MonsterAtkDeck.toEntity(atkDeck, monster));
+        });
+    }
 
 }
