1a0084651ede53d7be33f4fcf3aca64621bcaa46
joshua <joshuara7235@gmail.com>
2022-11-23 15:31:01 +0900

    feat: 공덱 업데이트 로직

--- src/main/java/com/joshua/summonerswar/domain/siege/service/core/relation/RelMonsterAtkDeckService.java
+++ src/main/java/com/joshua/summonerswar/domain/siege/service/core/relation/RelMonsterAtkDeckService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -21,10 +22,24 @@ public class RelMonsterAtkDeckService {
 
     private final RelMonsterAtkDeckRepository relMonsterAtkDeckRepository;
 
-    public void registerDefDeck(AtkDeck atkDeck, List<Monster> monsters) {
+    public void registerAtkDeck(AtkDeck atkDeck, List<Monster> monsters) {
         monsters.forEach(monster -> {
             relMonsterAtkDeckRepository.save(MonsterAtkDeck.toEntity(atkDeck, monster));
         });
+    }
+
+    public void updateAtkDeck(AtkDeck atkDeck, List<Monster> monsters) {
+
+        deleteRelation(atkDeck);
+
+        monsters.forEach(monster -> {
+            relMonsterAtkDeckRepository.save(MonsterAtkDeck.toEntity(atkDeck, monster));
+        });
+    }
+
+    private void deleteRelation (AtkDeck atkDeck) {
+        List<MonsterAtkDeck> monsterDefDeckList = relMonsterAtkDeckRepository.findByAtkDeckId(atkDeck.getId());
+        relMonsterAtkDeckRepository.deleteAllById(monsterDefDeckList.stream().map(MonsterAtkDeck::getId).collect(Collectors.toList()));
     }
 
 }
