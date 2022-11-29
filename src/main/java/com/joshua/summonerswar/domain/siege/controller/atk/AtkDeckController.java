ebc26aabdefe9d39d5fe502061035c923d4ed61a
joshua <joshuara7235@gmail.com>
2022-11-29 21:52:13 +0900

    feat: atk list controller

--- src/main/java/com/joshua/summonerswar/domain/siege/controller/atk/AtkDeckController.java
+++ src/main/java/com/joshua/summonerswar/domain/siege/controller/atk/AtkDeckController.java
@@ -7,7 +7,10 @@ import com.joshua.summonerswar.domain.siege.service.AtkDeckFacade;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,6 +25,14 @@ public class AtkDeckController {
     private final AtkDeckFacade atkDeckFacade;
 
     private final MonsterManagerFacade monsterManagerFacade;
+
+    @GetMapping ("/atkDecks")
+    public String viewList (Authentication authentication, Model model) {
+
+        model.addAttribute("monsters", monsterManagerFacade.findAll());
+
+        return "atk/list";
+    }
 
 
 
