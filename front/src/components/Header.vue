d861e91811872e048532404b95d19155bba64564
mingj7235 <joshuara7235@gmail.com>
2023-08-07 13:10:45 +0900

    feat : vue js 프로젝트 init (issue #1)

---
+++ front/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-header class="header">
+    <nav>
+      <el-menu mode="horizontal" router>
+        <el-menu-item index="/">Home</el-menu-item>
+        <el-menu-item index="/write">글 작성</el-menu-item>
+      </el-menu>
+    </nav>
+  </el-header>
+</template>
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.header {
+  padding: 0;
+  height: 60px;
+}
+</style>
---
+++ front/src/main.ts
@@ -0,0 +1,19 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+import "bootstrap/dist/css/bootstrap-utilities.css"
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(ElementPlus)
+
+app.mount('#app')
